e7468e084c3a593173c0709c5e96b28ab0a518f6
Charles HD <charleshdespointes@gmail.com>
2016-05-13 18:58:59 +0200

    add binary context behavior when no args provided

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,4 @@ main :: IO ()
 main = do args <- getArgs
           if length args > 1
              then paletteThem (args !! 0) (args !! 1)
-             else putStrLn "params should be paletteFile Directory"
+             else paletteThem "palette.bmp" "."
